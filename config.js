ff7804faefd06631f5bd0e02b1de46191b604ac1
Alessio Proietti <alessio.proietti@protonmail.com>
2020-08-22 10:47:37 +0200

    si va in scena!

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key: '3iIY9YxCjHSAUlZH5s0adD2SC',
+    consumer_secret: 'OZAgx4XajsfKmpqMcVzeCQUkc2I7qSpk6IU4zXtrUSruVfSuXV',
+    access_token: '1297076897252245504-eSWS0TfPtgVKdxXEv0TgrNRPMqNSed',
+    access_token_secret: 'q0Ll2YQincAvADd4t9BJQIZYZ0pVB7Cxf76cA0AlFqHTk',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+}
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+const config = require('./config')
+const T = require('twit')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+  T.post('statuses/update', { status: 'Ciao Amici!' }, function(err, data, response) {
+    console.log(data)
+  })
 })
 
 app.listen(process.env.PORT || 3000,
