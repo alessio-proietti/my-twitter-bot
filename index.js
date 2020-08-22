f7609aaccfea951c7762af38d9ee72c78c1fd70e
Alessio Proietti <alessio.proietti@protonmail.com>
2020-08-22 11:59:43 +0200

    si va in scena!

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const Twitter = require('twitter')
 const T = new Twitter(config)
 
 app.get('/', (req, res) => {
-  T.post('statuses/update', { status: 'Ciao Amici!' }, function (err, tweet, response) {
-    res.send({
-      data: tweet,
-      response: response,
-      hello: 'world'
-    })
-  })
+  res.send('Ciao Amici!')
 })
+
+var cron = require('node-cron');
+
+cron.schedule('* * * * *', () => {
+  T.post('statuses/update', { status: 'Ciao Amici! Ecco un numero random: \n ' + Math.random() }, function (err, tweet, response) {
+    console.log('running a task every minute');
+  })
+});
 
 app.listen(process.env.PORT || 3000,
   () => console.log("Server is running..."));
