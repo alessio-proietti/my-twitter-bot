5c195e4a8a9be4299c33521507b293acdb951c91
Alessio Proietti <alessio.proietti@protonmail.com>
2020-08-22 11:28:48 +0200

    si va in scena!

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key: '3iIY9YxCjHSAUlZH5s0adD2SC',
-    consumer_secret: 'OZAgx4XajsfKmpqMcVzeCQUkc2I7qSpk6IU4zXtrUSruVfSuXV',
+    consumer_key: 'YEAntPW59Xk3LKFBGi6U49Nqy',
+    consumer_secret: '7iKMS5BkHGvkWyk2LSBXS6fejhQ6YykaUjuXw2glljcP0RLRjr',
     access_token_key: '1297076897252245504-eSWS0TfPtgVKdxXEv0TgrNRPMqNSed',
     access_token_secret: 'q0Ll2YQincAvADd4t9BJQIZYZ0pVB7Cxf76cA0AlFqHTk'
 }
