6c0d0c63585daba6470d6abe3b51748bfd22100d
Alessio Proietti <alessio.proietti@protonmail.com>
2020-08-27 11:26:20 +0200

    exploring twilio API

--- config.js
+++ config.js
@@ -7,8 +7,17 @@ module.exports = {
         consumer_key: process.env.CONSUMER_KEY,
         consumer_secret: process.env.CONSUMER_SECRET,
         access_token_key: process.env.ACCESS_TOKEN_KEY,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
     },
+    twilioApi: {
+        accountSid:process.env.TWILIO_ACCOUNT_SID,
+        authToken:process.env.TWILIO_AUTH_TOKEN,
+    },
+    number: {
+        to: process.env.TWILIO_NUMBER_TO,
+        from:process.env.TWILIO_NUMBER_FROM,
+    },
+    twiliobody: "Ciao Alessio! Questo messaggio lo hai scritto pochi minuti fa tu sul tuo editor preferito. Bye Sarchiapone :)"
     oai: "http://www.openarchives.org/OAI/2.0/",
     arxiv: "http://arxiv.org/OAI/arXiv/"
 }
--- index.js
+++ index.js
@@ -8,16 +8,26 @@ const config = require('./config')
 const Twitter = require('twitter')
 const cron = require('node-cron')
 var buildUrl = require('build-url');
-
-
+const client = require('twilio')(accountSid, authToken);
 const T = new Twitter(config.twitterApi)
 
+
+const accountSid = config.accountSid;
+const authToken = config.authToken;
+
+
+
 app.get('/', (req, res) => {
-  T.post('statuses/update', {status: 'I Love Twitter'},  function(error, tweet, response) {
-    if(error) throw error;
+  T.post('statuses/update', { status: 'Hello Master! This is one of my test messages.' }, function (error, tweet, response) {
+    if (error) throw error;
     res.send('Ciao Amici!')
     console.log(tweet);  // Tweet body.
     console.log(response);  // Raw response object.
+
+
+    client.messages
+      .create({ body: config.twiliobody, from: config.number.from, to: config.number.to })
+      .then(message => console.log(message.sid));
   });
 })
 
